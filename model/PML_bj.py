3b6a530cb26cd73ca179ea384f78f873e09a7be0
swufe-NiceLab-GeoText <nicelab_geotex@163.com>
2026-05-12 16:08:16 +0800

    Update PML_bj.py

--- model/PML_bj.py
+++ model/PML_bj.py
@@ -156,7 +156,7 @@ class SpatialDecoder(nn.Module):
 class PML(nn.Module):
     def __init__(self, height=32, width=32, use_exf=False, scale_factor=4,
                  channels=128, sub_region=4, scaler_X=1, scaler_Y=1, args=None):
-        super(UNO, self).__init__()
+        super(PML, self).__init__()
         self.height = height
         self.width = width
         self.masker = SpatialMask(mask_ratio=0.75, patch_size=8)
